df5b93330da67d39bda2f03776053844cfb7a886
Paweł Wielkoliński <pawelw27@gmail.com>
2018-12-01 10:45:51 -0600

    array, section 2

---
+++ src/main/c02/js/array-constructor.js
@@ -0,0 +1,6 @@
+var colors = new Array('white',
+'black',
+'other');
+
+var el = document.getElementById('colors');
+el.innerHTML = colors.item(0); //innerHTML sets or returns HTML content
---
+++ src/main/c02/js/array-literal.js
@@ -0,0 +1,5 @@
+var colors;
+colors = ['white', 'black', 'other'];
+
+var el = document.getElementById('colors');
+el.textContent = colors[0]; //returns text content (replace all child nodes)
