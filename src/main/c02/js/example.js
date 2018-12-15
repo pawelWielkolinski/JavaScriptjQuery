b15fa9eb47c856e3e37c2f6a62c081e1c2f34b15
Paweł Wielkoliński <pawelw27@gmail.com>
2018-12-15 09:45:19 -0600

    conclusion, section 2

---
+++ src/main/c02/js/example.js
@@ -0,0 +1,29 @@
+var greeting = "Hi, ";
+var name = "Esther";
+var message = ", please check your order:";
+
+var welcome = greeting + name + message;
+
+var sign = "Montague House";
+var tiles = sign.length;
+var subTotal = tiles * 5;
+var shipping = 7;
+var grandTotal = subTotal + shipping;
+
+var el = document.getElementById("greeting");
+el.textContent = welcome;
+
+var elSign = document.getElementById("userSign");
+elSign.textContent = sign;
+
+var elTiles = document.getElementById("tiles");
+elTiles.textContent = tiles;
+
+var elSubTotal = document.getElementById("subTotal");
+elSubTotal.textContent = subTotal + " zl";
+
+var elSubTotal = document.getElementById("shipping");
+elSubTotal.textContent = shipping + " zl";
+
+var elGrandTotal = document.getElementById("grandTotal");
+elGrandTotal.textContent = grandTotal + " zl";
